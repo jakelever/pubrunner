638c5709960ae78d1541ee79cb23daec399f2e8b
Jake Lever <jake.lever@gmail.com>
2017-09-06 15:12:05 -0400

    Add a simple test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,2 +1,9 @@
+import sys
 import pubrunner
+from pubrunner.command_line import main
+
+def test_countwords():
+	sys.argv = ['pubrunner', '--test', 'tests/testrepos/CountWords/']
+	main()
+	# Check that things have happened
 
