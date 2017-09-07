54678fb9a6cbb4dec319ac98882d0d28a445d69c
Jake Lever <jake.lever@gmail.com>
2017-09-06 21:40:47 -0400

    Another coverage idea

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,7 +2,8 @@ import sys
 import pubrunner
 
 def test_countwords():
-	sys.argv = ['pubrunner', '--test', 'tests/testrepos/CountWords/']
-	pubrunner.main()
+	#sys.argv = ['pubrunner', '--test', 'tests/testrepos/CountWords/']
+	#pubrunner.main()
+	pubrunner.pubrun('tests/testrepos/CountWords/',True)
 	# Check that things have happened
 
