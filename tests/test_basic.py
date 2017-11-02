7cde7a232a53138c2c199c16f55e0aecdcbf1aee
Jake Lever <jlever@bcgsc.ca>
2017-11-02 03:00:42 -0700

    Test wasn't using --test flag properly

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,6 +4,6 @@ import pubrunner.command_line
 
 def test_countwords():
 	#pubrunner.pubrun('examples/CountWords/',True,True)
-	sys.argv = ['--test','examples/CountWords/']
+	sys.argv = ['pubrunner', '--test','examples/CountWords/']
 	pubrunner.command_line.main()
 
