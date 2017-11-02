2b249d8a81c51d30d9175ac033c7a0b208684d59
Jake Lever <jlever@bcgsc.ca>
2017-11-02 02:47:53 -0700

    Test case now runs main directly

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,9 @@
 import sys
 import pubrunner
+import pubrunner.command_line
 
 def test_countwords():
-	pubrunner.pubrun('examples/CountWords/',True,True)
+	#pubrunner.pubrun('examples/CountWords/',True,True)
+	sys.argv = ['--test','examples/CountWords/']
+	pubrunner.command_line.main()
 
