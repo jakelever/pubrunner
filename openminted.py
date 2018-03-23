3cbc29318a42a154d5243454df52d9ef2067870f
Jake Lever <jlever@bcgsc.ca>
2018-03-22 19:29:05 -0700

    Lowercased inputFormat and outputFormat for OpenMinTeD

--- openminted.py
+++ openminted.py
@@ -7,15 +7,15 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Main access point for OpenMinTeD Docker component')
 	parser.add_argument('--input',required=True,type=str,help='Input directory')
 	parser.add_argument('--output',required=True,type=str,help='Output directory')
-	parser.add_argument('--param:inputFormat',required=True,type=str,help='Input format')
-	parser.add_argument('--param:outputFormat',required=True,type=str,help='Output format')
+	parser.add_argument('--param:inputformat',required=True,type=str,help='Input format')
+	parser.add_argument('--param:outputformat',required=True,type=str,help='Output format')
 	args = parser.parse_args()
 
 	assert os.path.isdir(args.input)
 	assert os.path.isdir(args.output)
 
-	inputFormat = args.__dict__['param:inputFormat']
-	outputFormat = args.__dict__['param:outputFormat']
+	inputFormat = args.__dict__['param:inputformat']
+	outputFormat = args.__dict__['param:outputformat']
 
 	for f in os.listdir(args.input):
 		inFiles = [os.path.join(args.input,f)]
--- tests/docker.sh
+++ tests/docker.sh
@@ -7,8 +7,8 @@ hostoutput=$PWD/output
 containerinput=/input
 containeroutput=/output
 
-inputFormat=uimaxmi
-outputFormat=bioc
+inputformat=uimaxmi
+outputformat=bioc
 
 mkdir -p $hostinput
 mkdir -p $hostoutput
@@ -20,4 +20,4 @@ rm typesystem.xml
 rm XMIsamplesNew.zip
 cd -
 
-docker run -v $hostinput:$containerinput -v $hostoutput:$containeroutput jakelever/pubrunner $command --input $containerinput --output $containeroutput --param:inputFormat $inputFormat --param:outputFormat $outputFormat
+docker run -v $hostinput:$containerinput -v $hostoutput:$containeroutput jakelever/pubrunner $command --input $containerinput --output $containeroutput --param:inputformat $inputformat --param:outputformat $outputformat
