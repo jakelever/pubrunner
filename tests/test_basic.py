a0b2990479c094850157cd085dab7f1379dd1e2c
Jake Lever <jlever@bcgsc.ca>
2018-03-29 02:11:38 -0700

    Updated tests to use eutils for mini test resources

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,17 @@
 import sys
 import pubrunner
 import pubrunner.command_line
+import os
 
 def test_countwords():
-	parentDir = os.path.dirname(os.path.abspath(__file__))
+	parentDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 	projectPath = os.path.join(parentDir,'examples','CountWords')
 	sys.argv = ['pubrunner', '--defaultsettings', '--test',projectPath]
 	pubrunner.command_line.main()
 
 def test_textminingcounter():
-	parentDir = os.path.dirname(os.path.abspath(__file__))
-	projectPath = os.path.join(parentDir,'examples','TextMiningCount')
+	parentDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	projectPath = os.path.join(parentDir,'examples','TextMiningCounter')
 	sys.argv = ['pubrunner', '--defaultsettings', '--test',projectPath]
 	pubrunner.command_line.main()
 
