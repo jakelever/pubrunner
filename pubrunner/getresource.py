315184f3c3df9adee1a803d87115b864715dfba0
Jake Lever <jlever@bcgsc.ca>
2018-11-05 21:24:56 -0800

    Try reconnecting after each fail

--- pubrunner/getresource.py
+++ pubrunner/getresource.py
@@ -35,51 +35,51 @@ def download(url,out,fileSuffixFilter=None):
 		url = url.replace("ftp://","")
 		hostname = url.split('/')[0]
 		path = "/".join(url.split('/')[1:])
-		with ftputil.FTPHost(hostname, 'anonymous', 'secret') as host:
-			downloadFTP(path,out,host,fileSuffixFilter)
+		downloadFTP(path,out,hostname,fileSuffixFilter)
 	elif url.startswith('http'):
 		downloadHTTP(url,out,fileSuffixFilter)
 	else:
 		raise RuntimeError("Unsure how to download file. Expecting URL to start with ftp or http. Got: %s" % url)
 
-def downloadFTP(path,out,host,fileSuffixFilter=None,tries=5):
+def downloadFTP(path,out,hostname,fileSuffixFilter=None,tries=5):
 	for tryNo in range(tries):
 		try:
-			if host.path.isfile(path):
-				remoteTimestamp = host.path.getmtime(path)
-				
-				doDownload = True
-				if not checkFileSuffixFilter(path,fileSuffixFilter):
-					doDownload = False
-
-				if os.path.isdir(out):
-					localTimestamp = os.path.getmtime(out)
-					if not remoteTimestamp > localTimestamp:
+			with ftputil.FTPHost(hostname, 'anonymous', 'secret') as host:
+				if host.path.isfile(path):
+					remoteTimestamp = host.path.getmtime(path)
+					
+					doDownload = True
+					if not checkFileSuffixFilter(path,fileSuffixFilter):
 						doDownload = False
-				if path.endswith('.gz'):
-					outUnzipped = out[:-3]
-					if os.path.isfile(outUnzipped):
-						localTimestamp = os.path.getmtime(outUnzipped)
+
+					if os.path.isdir(out):
+						localTimestamp = os.path.getmtime(out)
 						if not remoteTimestamp > localTimestamp:
 							doDownload = False
-				if doDownload:
-					print("  Downloading %s" % path)
-					host.download(path,out)
-					os.utime(out,(remoteTimestamp,remoteTimestamp))
-				else:
-					print("  Skipping %s" % path)
+					if path.endswith('.gz'):
+						outUnzipped = out[:-3]
+						if os.path.isfile(outUnzipped):
+							localTimestamp = os.path.getmtime(outUnzipped)
+							if not remoteTimestamp > localTimestamp:
+								doDownload = False
+					if doDownload:
+						print("  Downloading %s" % path)
+						host.download(path,out)
+						os.utime(out,(remoteTimestamp,remoteTimestamp))
+					else:
+						print("  Skipping %s" % path)
 
-			elif host.path.isdir(path):
-				basename = host.path.basename(path)
-				newOut = os.path.join(out,basename)
-				if not os.path.isdir(newOut):
-					os.makedirs(newOut)
-				for child in host.listdir(path):
-					srcFilename = host.path.join(path,child)
-					dstFilename = os.path.join(newOut,child)
-					downloadFTP(srcFilename,dstFilename,host,fileSuffixFilter)
-			else:
-				raise RuntimeError("Path (%s) is not a file or directory" % path) 
+				elif host.path.isdir(path):
+					basename = host.path.basename(path)
+					newOut = os.path.join(out,basename)
+					if not os.path.isdir(newOut):
+						os.makedirs(newOut)
+					for child in host.listdir(path):
+						srcFilename = host.path.join(path,child)
+						dstFilename = os.path.join(newOut,child)
+						downloadFTP(srcFilename,dstFilename,host,fileSuffixFilter)
+				else:
+					raise RuntimeError("Path (%s) is not a file or directory" % path) 
 
 			break
 		except ftputil.error.FTPOSError as e:
