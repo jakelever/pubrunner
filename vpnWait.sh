163f2f0214f80993ce92ed803fad82c256c0b4c2
Jake Lever <jlever@bcgsc.ca>
2019-04-26 16:32:01 -0700

    Try DNS fix

--- vpnWait.sh
+++ vpnWait.sh
@@ -10,6 +10,7 @@ do
 
 	#if grep -q "Initialization Sequence Completed" /var/log/openvpn.log; then
 	if grep -q "/sbin/ip addr add dev" /var/log/openvpn.log; then
+		sleep 5
 		exit 0
 	fi
 	sleep 5
