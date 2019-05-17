8fcb85124518327ffe00956658d4e34f3d8c5bb6
Jake Lever <jlever@bcgsc.ca>
2019-05-16 19:10:22 -0700

    Memory fix for gatherPMIDs

--- pubrunner/gather_pmids.py
+++ pubrunner/gather_pmids.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import json
-from collections import defaultdict
+from collections import defaultdict,Counter
 import pubrunner
 
 def gatherPMIDs(inHashDir,outPMIDDir,whichHashes=None,pmidExclusions=None):
@@ -9,35 +9,60 @@ def gatherPMIDs(inHashDir,outPMIDDir,whichHashes=None,pmidExclusions=None):
 	if os.path.isdir(outPMIDDir):
 		inHashDir_modifieds = [ os.path.getmtime(os.path.join(root,f)) for root, dir, files in os.walk(inHashDir) for f in files ]
 		outPMIDDir_modifieds = [ os.path.getmtime(os.path.join(root,f)) for root, dir, files in os.walk(inHashDir) for f in files ]
-	#	print("max(inHashDir_modifieds)",max(inHashDir_modifieds))
-	#	print("max(outPMIDDir_modifieds)",max(outPMIDDir_modifieds))
 		if max(inHashDir_modifieds) < max(outPMIDDir_modifieds):
 			print("No PMID update necessary")
 			return
 
 	files = sorted([ os.path.join(inHashDir,f) for f in os.listdir(inHashDir) ])
-	hashes = {}
+	pmidToFilename = {}
+
+	pubmedXMLFiles = []
+	firstFile = {}
+	versionCounts = Counter()
 	for filename in files:
 		with open(filename) as f:
-			tmpHashes = json.load(f)
-			hashes.update(tmpHashes)
+			hashes = json.load(f)
+			keys = list(hashes.keys())
+			assert len(keys) == 1
+			pubmedXMLFile = keys[0]
+			pubmedXMLFiles.append(pubmedXMLFile)
 
+		for pmid in hashes[pubmedXMLFile].keys():
+			pmidInt = int(pmid)
+			if not pmidInt in firstFile:
+				firstFile[pmidInt] = pubmedXMLFile
+			versionCounts[pmidInt] += 1
+
+	# Set up the filenames for PMIDs with only one version (which is the vast majority)
+	pmidToFilename = { pmid:firstFile[pmid] for pmid,count in versionCounts.items() if count == 1 }
+	pmidsToSkip = set(pmidToFilename.keys())
+
+	# Now we iterate through (looking at only those with multiple versions) and check if the hashes change
 	pmidHashes = defaultdict(list)
-	pmidToFilename = {}
-	for filename in sorted(hashes.keys()):
-		for pmid in hashes[filename].keys():
+	for filename in sorted(files):
+		with open(filename) as f:
+			hashes = json.load(f)
+			keys = list(hashes.keys())
+			assert len(keys) == 1
+			pubmedXMLFile = keys[0]
+
+		for pmid in hashes[pubmedXMLFile].keys():
+			pmidInt = int(pmid)
+			if pmidInt in pmidsToSkip:
+				continue
+
 			if whichHashes is None:
-				hashVal = hashes[filename][pmid]
+				hashVal = hashes[pubmedXMLFile][pmid]
 			else:
 				try:
-					hashVal = [ hashes[filename][pmid][h] for h in whichHashes ]
+					hashVal = [ hashes[pubmedXMLFile][pmid][h] for h in whichHashes ]
 				except KeyError as e:
 					raise RuntimeError("The selected hash (%s) from the 'usePubmedHashes' option has not been found in the hash files." % (str(e)))
 
-			pmidInt = int(pmid)
 			if pmidHashes[pmidInt] != hashVal:
 				pmidHashes[pmidInt] = hashVal
-				pmidToFilename[pmidInt] = filename
+				pmidToFilename[pmidInt] = pubmedXMLFile
+
 
 	filenameToPMIDs = defaultdict(list)
 	for pmid,filename in pmidToFilename.items():
@@ -46,10 +71,14 @@ def gatherPMIDs(inHashDir,outPMIDDir,whichHashes=None,pmidExclusions=None):
 	if not os.path.isdir(outPMIDDir):
 		os.makedirs(outPMIDDir)
 
-	for filename in sorted(hashes.keys()):
+	for filename in pubmedXMLFiles:
 		basename = os.path.basename(filename)
 		outName = os.path.join(outPMIDDir,basename+'.pmids')
-		pmids = sorted(filenameToPMIDs[filename])
+
+		if filename in filenameToPMIDs:
+			pmids = sorted(filenameToPMIDs[filename])
+		else:
+			pmids = []
 
 		if not pmidExclusions is None:
 			pmids = [ pmid for pmid in pmids if not pmid in pmidExclusions ]
